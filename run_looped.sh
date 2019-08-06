c31db8776f6cb3cbebd70219a7011f41f9d401c4
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2019-08-06 14:32:34 -0400

    Removed unnecessary files and binaries

--- run_looped.sh
+++ run_looped.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-printf "0        10        20        30        40        50        60        70        80        90        100"
 
+cp hello_world hello_world_copy
+printf "0        10        20        30        40        50        60        70        80        90        100"
 for i in {0..1000} ; do
 	if [[ $(($i % 10)) == 0 ]] ; then
 		printf "."
@@ -10,7 +11,6 @@ for i in {0..1000} ; do
 	if [[ $(($i % 100)) == 0 ]] ; then 
 		printf "\n"
 	fi
-	#cp hello_world hello_world_copy
-	#./run_obf.sh >> looped.log
-	#mv hello_world_mal hello_world
+	./run_obf.sh >> looped.log
+	mv hello_world_mal hello_world
 done
