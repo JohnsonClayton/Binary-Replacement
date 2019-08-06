2205e1dda94ecb8938801e143cc652144c9c1fcf
Clayton Johnson <cpjohnson@mavs.coloradomesa.edu>
2019-08-06 11:04:52 -0600

    Added looping functionality

--- run_looped.sh
+++ run_looped.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
+printf "0        10        20        30        40        50        60        70        80        90        100"
 
-for i in {1..1000} ; do
+for i in {0..1000} ; do
 	if [[ $(($i % 10)) == 0 ]] ; then
-		echo "$i"
+		printf "."
+	else
+		printf "."
+	fi	
+	if [[ $(($i % 100)) == 0 ]] ; then 
+		printf "\n"
 	fi
-./run_obf.sh >> output_file.txt
+	#cp hello_world hello_world_copy
+	#./run_obf.sh >> looped.log
+	#mv hello_world_mal hello_world
 done
